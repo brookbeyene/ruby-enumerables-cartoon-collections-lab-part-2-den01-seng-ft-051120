c554e200cedaaeb8aaa03b0b51047e09d5ca93dc
Brook Beyene <brookbeyene37@gmail.com>
2020-05-02 05:10:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  new_array = planeteer_calls.map{|n.length > 1| true}
+  new_array = planeteer_calls.map{|(n.length > 1)| true}
   
 end
 
